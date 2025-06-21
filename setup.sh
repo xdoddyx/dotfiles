38ab72c200269155ee004ae36223074b30580ec8
Doddy Setyawan <xdoddyx@yahoo.com>
2025-06-22 00:42:52 +0700

    update

--- setup.sh
+++ setup.sh
@@ -2,6 +2,11 @@
 
 set -euo pipefail
 
+# ensure ~/.ssh exists and trust GitHub’s key
+mkdir -p ~/.ssh
+chmod 700 ~/.ssh
+ssh-keyscan github.com >> ~/.ssh/known_hosts
+
 if ! command -v chezmoi >/dev/null; then
     sh -c "$(curl -fsLS get.chezmoi.io)" -- init --apply git@github.com:xdoddyx/dotfiles.git
 fi
